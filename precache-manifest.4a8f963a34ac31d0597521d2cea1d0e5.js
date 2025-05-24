51a3ae18ea4a5121a295a495ea8c8d26604e0fc3
Vibhuti Dureja <vd20075834@gmail.com>
2025-05-24 22:46:15 +0530

    Updates

---
+++ precache-manifest.4a8f963a34ac31d0597521d2cea1d0e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a43be2c2ea3e7d7ad240a7d8b6ae67",
+    "url": "/TextUtils-React/index.html"
+  },
+  {
+    "revision": "369d105834455f402b44",
+    "url": "/TextUtils-React/static/css/main.0bcbf7ca.chunk.css"
+  },
+  {
+    "revision": "ec0935851c1edf4cb708",
+    "url": "/TextUtils-React/static/js/2.9e11b540.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/TextUtils-React/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "369d105834455f402b44",
+    "url": "/TextUtils-React/static/js/main.8b81e653.chunk.js"
+  },
+  {
+    "revision": "37d7f545d8875c1e6f67",
+    "url": "/TextUtils-React/static/js/runtime~main.688a2915.js"
+  }
+]);
